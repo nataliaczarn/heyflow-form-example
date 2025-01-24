e89f26f72ae1ac6a02fdf30908265ed5e7d28c57
Natalia Czarnecka <natalia.czarnecka@unic.com>
2025-01-24 23:13:40 +0100

    feat: test

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -35,6 +35,7 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         {children}
+        
       </body>
     </html>
   );
